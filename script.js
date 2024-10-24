df9f4cdd0457707b132085421946124c259e0037
Nimish <nimishmahajan49@gmail.com>
2024-10-24 16:06:16 +0530

    Your commie

--- script.js
+++ script.js
@@ -5,20 +5,3 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-function hover(){
-  const xebia =document.querySelector(".xebia");
-  const thapar =document.querySelector(".tiet");
-  const college =document.querySelector(".details-container1");
-  const company =document.querySelector(".details-container2");
-
-college.addEventListener('mouseover',function(){
- xebia.style.display='block';
- thapar.style.display='none';
-});
-
-college.addEventListener('mouseout',function(){
-  xebia.style.display='none';
-  thapar.style.display='block';
- });
-  
-}
